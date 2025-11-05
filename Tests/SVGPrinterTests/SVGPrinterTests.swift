7b48711f77bbc9b7d534b54d90e9872c7a315ff8
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-11-05 13:51:01 +0100

    chore: apply swift-format with 4-space indentation

--- Tests/SVGPrinterTests/SVGPrinterTests.swift
+++ Tests/SVGPrinterTests/SVGPrinterTests.swift
@@ -13,41 +13,41 @@ import Testing
 @Suite("SVG Printer Tests")
 struct SVGPrinterTests {
 
-  @Test("Basic printer functionality")
-  func basicPrinter() {
-    var printer = SVGPrinter()
-    printer.append("<test>")
-    printer.append("content")
-    printer.append("</test>")
+    @Test("Basic printer functionality")
+    func basicPrinter() {
+        var printer = SVGPrinter()
+        printer.append("<test>")
+        printer.append("content")
+        printer.append("</test>")
 
-    let result = String(decoding: printer.bytes, as: UTF8.self)
-    #expect(result == "<test>content</test>")
-  }
+        let result = String(decoding: printer.bytes, as: UTF8.self)
+        #expect(result == "<test>content</test>")
+    }
 
-  @Test("Pretty printing")
-  func prettyPrinting() {
-    var printer = SVGPrinter(.pretty)
-    printer.append("<parent>")
-    printer.appendNewlineIfNeeded()
-    printer.indent()
-    printer.appendIndentationIfNeeded()
-    printer.append("<child />")
-    printer.outdent()
-    printer.appendNewlineIfNeeded()
-    printer.append("</parent>")
+    @Test("Pretty printing")
+    func prettyPrinting() {
+        var printer = SVGPrinter(.pretty)
+        printer.append("<parent>")
+        printer.appendNewlineIfNeeded()
+        printer.indent()
+        printer.appendIndentationIfNeeded()
+        printer.append("<child />")
+        printer.outdent()
+        printer.appendNewlineIfNeeded()
+        printer.append("</parent>")
 
-    let result = String(decoding: printer.bytes, as: UTF8.self)
-    #expect(result.contains("\n"))
-    #expect(result.contains("  "))  // indentation
-  }
+        let result = String(decoding: printer.bytes, as: UTF8.self)
+        #expect(result.contains("\n"))
+        #expect(result.contains("  "))  // indentation
+    }
 
-  @Test("Attribute collection")
-  func attributes() {
-    var printer = SVGPrinter()
-    printer.attributes["fill"] = "red"
-    printer.attributes["stroke"] = "black"
+    @Test("Attribute collection")
+    func attributes() {
+        var printer = SVGPrinter()
+        printer.attributes["fill"] = "red"
+        printer.attributes["stroke"] = "black"
 
-    #expect(printer.attributes["fill"] == "red")
-    #expect(printer.attributes["stroke"] == "black")
-  }
+        #expect(printer.attributes["fill"] == "red")
+        #expect(printer.attributes["stroke"] == "black")
+    }
 }
